24f3372de4fdae0e51247cdffd28257ba74b01c3
Sean Kermes <skermes@gmail.com>
2014-02-18 12:16:59 -0500

    Stub of app.

---
+++ server/editorialish.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var editorialish = express();
+
+editorialish.configure(function() {
+  editorialish.use(express.bodyParser());
+  editorialish.use(express.methodOverride());
+  editorialish.use(editorialish.router);
+  editorialish.use(express.errorHandler(
+    { dumpExceptions: true, showStack: true }));
+});
+
+editorialish.get('/', function(req, res) {
+  res.send('Hello, world');
+});
+
+var port = Number(process.env.PORT || 3000);
+editorialish.listen(port);
