4f6af199e42902ac5e954e61e954f0e131f1d699
Sean Kermes <skermes@gmail.com>
2014-02-19 17:56:44 -0500

    Remove test scripts.

--- client/src/scripts/mod.js
+++
@@ -1,9 +0,0 @@
-var Backbone = require('backbone');
-
-var Mod = Backbone.Model.extend({
-  defaults: {
-    title: "A Watched Model"
-  }
-});
-
-module.exports = Mod;
--- client/src/scripts/test.js
+++
@@ -1,6 +0,0 @@
-var Backbone = require('backbone'),
-    $ = require('jquery');
-Backbone.$ = $;
-
-var Mod = require('./mod');
-console.log(new Mod().get('title'));
