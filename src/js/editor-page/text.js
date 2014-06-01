6b442d5fd5faa5e5449ff0fb396450916c64a9f0
Sean Kermes <skermes@gmail.com>
2014-05-31 21:04:11 -0400

    Move componentDidMount up, for consistency.

--- src/js/editor-page/text.js
+++ src/js/editor-page/text.js
@@ -10,7 +10,16 @@ var TextEdit = React.createClass({
       text: this.props.text
     };
   },
-
+  componentDidMount: function() {
+    var text = this.refs.cmText.getDOMNode();
+    var editor = CodeMirror.fromTextArea(text,{
+      mode: 'markdown',
+      lineNumbers: false,
+      lineWrapping: true,
+      dragDrop:false,
+      autoFocus:true
+    });
+  },
   render: function() {
     return (
       <textarea ref='cmText' onChange={this._onChange} value={this.state.text} />
@@ -21,19 +30,7 @@ var TextEdit = React.createClass({
     Dispatcher.send('manuscript:localUpdate',
                     [this.props.manuscriptId, {text: event.target.value}]);
     this.setState({text: event.target.value});
-  },
-
-  componentDidMount: function() {
-    var text = this.refs.cmText.getDOMNode();
-    var editor = CodeMirror.fromTextArea(text,{
-      mode: 'markdown',
-      lineNumbers: false,
-      lineWrapping: true,
-      dragDrop:false,
-      autoFocus:true
-    });
   }
-
 });
 
 module.exports = TextEdit;
