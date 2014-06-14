b0268626dea3c7ac0fd39015884c561887195062
Sean Kermes <skermes@gmail.com>
2014-06-14 19:22:20 -0400

    Set data in menu store.

--- src/js/store/menu-store.js
+++ src/js/store/menu-store.js
@@ -2,11 +2,21 @@ var Store = require('./store');
 
 var MenuStore = new Store({
   initialize: function() {
-
+    this.items = undefined;
+    this.anchor = undefined;
   },
 
   dispatches: {
-
+    'menu:open': function(items, anchor) {
+      this.items = items;
+      this.anchor = anchor;
+      this.trigger();
+    },
+    'menu:close': function() {
+      this.items = undefined;
+      this.anchor = undefined;
+      this.trigger();
+    }
   }
 });
 
