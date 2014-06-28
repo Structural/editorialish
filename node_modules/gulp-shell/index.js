9b1096faca0bccea9c3af3d96e55aa87f2b9b404
Sean Kermes <skermes@gmail.com>
2014-06-28 18:18:07 -0400

    Install gulp-shell

---
+++ node_modules/gulp-shell/index.js
@@ -0,0 +1,79 @@
+var _       = require('lodash')
+var async   = require('async')
+var exec    = require('child_process').exec
+var gutil   = require('gulp-util')
+var join    = require('path').join
+var through = require('through2')
+
+var PLUGIN_NAME = 'gulp-shell'
+
+function shell(commands, options) {
+  if (typeof commands === 'string') {
+    commands = [commands]
+  }
+
+  if (!Array.isArray(commands)) {
+    throw new gutil.PluginError(PLUGIN_NAME, 'Missing commands')
+  }
+
+  options = _.extend({
+    ignoreErrors: false,
+    quiet: false,
+    cwd: process.cwd()
+  }, options)
+
+  var pathToBin = join(process.cwd(), 'node_modules/.bin')
+  var separator = process.platform.match(/^win/) ? ';' : ':'
+  var path = pathToBin + separator + process.env.PATH
+  var env = _.defaults({PATH: path}, process.env)
+
+  return through.obj(function (file, unused, done) {
+    var self = this
+
+    async.eachSeries(commands, function (command, done) {
+      command = gutil.template(command, {file: file})
+
+      var child = exec(command, {
+        env: env,
+        cwd: options.cwd,
+        maxBuffer: 16 * 1024 * 1024
+      }, function (error) {
+        process.stdin.removeListener('data', onData)
+        process.stdin.pause()
+
+        done(options.ignoreErrors ? null : error)
+      })
+
+      var onData = function (chunk) {
+        child.stdin.write(chunk)
+      }
+      process.stdin.on('data', onData)
+      process.stdin.resume()
+
+      if (!options.quiet) {
+        child.stdout.pipe(process.stdout)
+        child.stderr.pipe(process.stderr)
+      }
+    }, function (error) {
+      if (error) {
+        self.emit('error', new gutil.PluginError(PLUGIN_NAME, error))
+      } else {
+        self.push(file)
+      }
+      done()
+    })
+  })
+}
+
+shell.task = function (commands, options) {
+  return function () {
+    var stream = shell(commands, options)
+
+    stream.write(new gutil.File())
+    stream.end()
+
+    return stream
+  }
+}
+
+module.exports = shell
