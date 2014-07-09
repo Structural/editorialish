aa8e134a620be6296bf14ef7e834ff8d28c046cf
Sean Kermes <skermes@gmail.com>
2014-07-09 18:39:05 -0400

    In dev mode, Store holds a list of all stores.

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,36 +1,54 @@
-var _ = require('underscore'),
-    Dispatcher = require('../dispatcher/dispatcher');
+var _ = require('underscore');
 
-var Store = function(options) {
-  this._callbacks = [];
-  _.extend(this, _.omit(options, 'initialize', 'dispatches'));
+var Dispatcher = require('../dispatcher/dispatcher'),
+    Environment = require('../environment');
 
-  if (options.initialize) {
-    options.initialize.call(this);;
+var Store = (function(env) {
+  return function(options) {
+    if (env.DevMode) {
+      Store.stores.push(this);
+    }
+
+    this._callbacks = [];
+    _.extend(this, _.omit(options, 'initialize', 'dispatches'));
+
+    if (options.initialize) {
+      options.initialize.call(this);
+    }
+
+    if (options.dispatches) {
+      _.each(options.dispatches, function(callback, action) {
+        Dispatcher.on(action, callback, this);
+      }, this);
+    }
+
+    this.listen = function(callback, context) {
+      this._callbacks.push({callback: callback, context: context})
+    }
+
+    this.ignore = function(callback) {
+      this._callbacks = _.reject(this._callbacks, function(cb) {
+        return cb.callback === callback;
+      });
+    }
+
+    this.trigger = function() {
+      _.each(this._callbacks, function(cb) {
+        var fn = cb.context ? cb.callback.bind(cb.context) : cb.callback;
+        fn();
+      });
+    }
   }
+})(Environment);
+/* I swear, I have *no* *idea* why Store is closing over _ and Dispatcher but
+   not Environment.  Environment is in the local scope when Store is declared,
+   but Chrome's dev tools don't show it in the closure scope when Store is
+   evaluated.  Reading
+   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions_and_function_scope#Nested_functions_and_closures
+   did not help.  So, fuck it I guess.  IIFE FTW. */
 
-  if (options.dispatches) {
-    _.each(options.dispatches, function(callback, action) {
-      Dispatcher.on(action, callback, this);
-    }, this);
-  }
-
-  this.listen = function(callback, context) {
-    this._callbacks.push({callback: callback, context: context})
-  }
-
-  this.ignore = function(callback) {
-    this._callbacks = _.reject(this._callbacks, function(cb) {
-      return cb.callback === callback;
-    });
-  }
-
-  this.trigger = function() {
-    _.each(this._callbacks, function(cb) {
-      var fn = cb.context ? cb.callback.bind(cb.context) : cb.callback;
-      fn();
-    });
-  }
-};
+if (Environment.DevMode) {
+  Store.stores = [];
+}
 
 module.exports = Store;
